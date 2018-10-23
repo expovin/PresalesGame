12c3bf06b851cdf20f7ff843055ea4bf613e4f32
Vincenzo Esposito <expovin@gmail.com>
2018-10-23 21:34:05 +0200

    Opportunities refactored

--- routes/opportunities.js
+++ routes/opportunities.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var Oppy = require('../lib/ClassOpportunities');
+var m = require('../lib/Game');
+
 var router = express.Router();
 
-var OpportunitiesList=[];
+//var OpportunitiesList=[];
 
 /* get all presales */
 router.route('/')
 .get( function(req, res, next) {
-    var oppys=[];
-    OpportunitiesList.forEach( o => oppys.push(o.getValues()));
-    res.status(200).json({result:'OK', data: oppys});
+    res.status(200).json({result:'OK', data: m[req.headers.gameid].getOpportunities()});
 })
 .post( function (req, res, next){
     for(var i=0; i<req.body.num; i++)
-        OpportunitiesList.push(new Oppy(req.body.minValue, req.body.maxValue, req.body.minTTC, req.body.maxTTC))
+        m[req.headers.gameid].addOpportunity(new Oppy(req.body.minValue, req.body.maxValue, req.body.minTTC, req.body.maxTTC))
 
     res.status(200).json({result:'OK', message:'Generated '+req.body.num+' opportunities'});
 })
@@ -21,13 +21,13 @@ router.route('/')
     res.status(209).json({result:'WARNING', message:'This function has not been implemented yet'});
 })
 .delete( function (req, res, next){
-    OpportunitiesList=[];
+    m[req.headers.gameid].deleteOpportunities();
     res.status(200).json({result:'OK', message:'Opportunities have been deleted'});
 })
 
 router.route('/:oppyID')
 .get( function(req, res, next) {
-    res.status(200).json({result:'OK', data: OpportunitiesList[req.params.oppyID].getValues()});
+    res.status(209).json({result:'WARNING', message:'This function has not been implemented yet'});
 })
 .post( function (req, res, next){
     res.status(209).json({result:'WARNING', message:'This function has not been implemented yet'});
