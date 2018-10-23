dafe6d7a10452753d62c1b3f65d5236d8316a9b6
Vincenzo Esposito <expovin@gmail.com>
2018-10-23 21:38:11 +0200

    Companies refactored

--- routes/company.js
+++ routes/company.js
@@ -3,29 +3,28 @@ var Company = require('../lib/ClassCompany');
 var m = require('../lib/Game');
 var router = express.Router();
 
-//var m= new Market();
 
 /* get all presales */
 router.route('/')
 .get( function(req, res, next) {
-    res.status(200).json({result:'OK', data: m['m'].getCompanies()});
+    res.status(200).json({result:'OK', data: m[req.headers.gameid].getCompanies()});
 })
 .post( function (req, res, next){
     var c = new Company(req.body.name);
-    m['m'].addCompany(c);
+    m[req.headers.gameid].addCompany(c);
     res.status(200).json({result:'OK', message:'Added company '+req.body.name, id : c.getID()});
 })
 .put( function (req, res, next){
     res.status(209).json({result:'WARNING', message:'This function has not been implemented yet'});
 })
 .delete( function (req, res, next){
-    m['m'].deleteCompanies();
+    m[req.headers.gameid].deleteCompanies();
     res.status(200).json({result:'OK', message:'Companies have been deleted'});
 })
 
 router.route('/:companyID')
 .get( function(req, res, next) {
-    res.status(200).json({result:'OK', data: m['m'].getCompany(req.params.companyID)});
+    res.status(200).json({result:'OK', data: m[req.headers.gameid].getCompany(req.params.companyID)});
 })
 .post( function (req, res, next){
     res.status(209).json({result:'WARNING', message:'This function has not been implemented yet'});
@@ -39,17 +38,17 @@ router.route('/:companyID')
 
 router.route('/:companyID/message')
 .get( function(req, res, next) {
-    res.status(200).json({result:'OK', data: m['m'].getCompany(req.params.companyID).getMessage()});
+    res.status(200).json({result:'OK', data: m[req.headers.gameid].getCompany(req.params.companyID).getMessage()});
 })
 .post( function (req, res, next){
-    m['m'].getCompany(req.params.companyID).sendMessage(req.body.message);
+    m[req.headers.gameid].getCompany(req.params.companyID).sendMessage(req.body.message);
     res.status(200).json({result:'OK', message:'Message sent'});
 })
 .put( function (req, res, next){
     res.status(209).json({result:'WARNING', message:'This function has not been implemented yet'});
 })
 .delete( function (req, res, next){
-    m['m'].getCompany(req.params.companyID).deleteMessages();
+    m[req.headers.gameid].getCompany(req.params.companyID).deleteMessages();
     res.status(200).json({result:'OK', message:'All messages have been deleted!'});
 })
 
