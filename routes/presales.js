e19dd82a3bf4d1dc700396bcedb8d0d97409ac0f
Vincenzo Esposito <expovin@gmail.com>
2018-10-23 21:24:23 +0200

    presales refactored

--- routes/presales.js
+++ routes/presales.js
@@ -4,17 +4,16 @@ var m = require('../lib/Game');
 
 var router = express.Router();
 
-var PresalesPeople={};
+//var PresalesPeople={};
 
 /* get all presales */
 router.route('/')
 .get( function(req, res, next) {
-    //PresalesPeople.forEach( p => people.push(p.getValues()));
-    res.status(200).json({result:'OK', data: m['m'].getPeople()});
+    res.status(200).json({result:'OK', data: m[req.headers.gameid].getPeople()});
 })
 .post( function (req, res, next){
     for(var i=0; i<req.body.num; i++){
-        var p = new Presales();
+        var p = new Presales(m[req.headers.gameid].getMarketTrends());
         m['m'].addPerson(p);
     }
     res.status(200).json({result:'OK', message:'Generated '+req.body.num+' presales person'});
@@ -23,13 +22,13 @@ router.route('/')
     res.status(209).json({result:'WARNING', message:'This function has not been implemented yet'});
 })
 .delete( function (req, res, next){
-    PresalesPeople={};
+    m['m'].deletePeople()
     res.status(200).json({result:'OK', message:'Presales people have been deleted'});
 })
 
 router.route('/:presalesID')
 .get( function(req, res, next) {
-    res.status(200).json({result:'OK', data: PresalesPeople[req.params.presalesID].getValues()});
+    res.status(200).json({result:'OK', data: m[req.headers.gameid].getPerson(req.params.presalesID)});
 })
 .post( function (req, res, next){
     res.status(209).json({result:'WARNING', message:'This function has not been implemented yet'});
