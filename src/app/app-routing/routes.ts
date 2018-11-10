ea39d1bfa7c03c01c53d527b83e389c84bd48529
Vincenzo Esposito <expovin@gmail.com>
2018-11-10 23:10:39 +0100

    Bootstrap tmplate Integrated

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { MarketComponent } from '../market/market.component';
+import { OpportunitiesComponent } from '../opportunities/opportunities.component';
+import  { PeopleComponent } from '../people/people.component';
+import  { BamComponent } from '../bam/bam.component';
+import  { TopComponent } from '../top/top.component';
+
+export const routes: Routes = [
+  { path: 'dashboard',  component: DashboardComponent },
+  { path: 'market',     component: MarketComponent },
+  { path: 'oppy',     component: OpportunitiesComponent },
+  { path: 'people',     component: PeopleComponent },
+  { path: 'bam',     component: BamComponent },
+  { path: 'top',     component: TopComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { MarketComponent } from './market/market.component';
+import { OpportunitiesComponent } from './opportunities/opportunities.component';
+import { PeopleComponent } from './people/people.component';
+import { BamComponent } from './bam/bam.component';
+import { TopComponent } from './top/top.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    DashboardComponent,
+    FooterComponent,
+    MarketComponent,
+    OpportunitiesComponent,
+    PeopleComponent,
+    BamComponent,
+    TopComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
